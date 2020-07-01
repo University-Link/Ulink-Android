3377e8c9cdcf6d4148436af0820b9ee3b210196d
leejieun1121 <lje4648@naver.com>
2020-07-01 22:43:32 +0900

    create

---
+++ app/src/main/java/com/example/ulink/ClassRecycler/ClassData.kt
@@ -0,0 +1,9 @@
+package com.example.ulink.ClassRecycler
+
+data class ClassData(
+    val ClassImage : String,
+    val ClassName : String,
+    val partition : Int,
+    val all : Int,
+    val count : Int
+)
