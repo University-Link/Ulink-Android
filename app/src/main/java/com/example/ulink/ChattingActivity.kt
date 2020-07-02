992799cd9deac8bbcdd79fc394919cd9a1be36fc
leejieun1121 <lje4648@naver.com>
2020-07-03 00:31:32 +0900

    햄버거바 기능 클릭 -> 연결

--- app/src/main/java/com/example/ulink/ChattingActivity.kt
+++ app/src/main/java/com/example/ulink/ChattingActivity.kt
@@ -1,7 +1,10 @@
 package com.example.ulink
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Gravity
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -9,7 +12,7 @@ import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_chatting.*
 import kotlinx.android.synthetic.main.toolbar.*
 
-class ChattingActivity : AppCompatActivity() {
+class ChattingActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatting)
@@ -17,6 +20,9 @@ class ChattingActivity : AppCompatActivity() {
         img_hamburger.setOnClickListener {
             setDrawerLayout(drawer_layout_main, nav_view as NavigationView)
         }
+        nav_view.setNavigationItemSelectedListener(this) //navigation 리스너
+
+
     }
     private fun setDrawerLayout(drawerLayout: DrawerLayout, navigationView: NavigationView){
         drawerLayout.openDrawer(Gravity.RIGHT)
@@ -26,8 +32,24 @@ class ChattingActivity : AppCompatActivity() {
                 it.closeDrawer(GravityCompat.END)
             }
         }
-
-
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.notice->{
+                val intent = Intent(this, NoticeActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.QnA-> Toast.makeText(this,"qna공간",Toast.LENGTH_SHORT).show()
+            R.id.picture-> Toast.makeText(this,"사진,동영상",Toast.LENGTH_SHORT).show()
+            R.id.logout-> Toast.makeText(this,"파",Toast.LENGTH_SHORT).show()
+
+        }
+        return false
+    }
+
+
+
+
 
 }
