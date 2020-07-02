2db792efa03553736528d5bc96aa8d02893c444c
leejieun1121 <lje4648@naver.com>
2020-07-02 23:03:53 +0900

    hamburger bar 기능 구현

--- app/src/main/java/com/example/ulink/ChattingActivity.kt
+++ app/src/main/java/com/example/ulink/ChattingActivity.kt
@@ -1,17 +1,33 @@
 package com.example.ulink
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_chatting.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class ChattingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatting)
 
-        btn_back.setOnClickListener{
-            finish()
+        img_hamburger.setOnClickListener {
+            setDrawerLayout(drawer_layout_main, nav_view as NavigationView)
         }
+    }
+    private fun setDrawerLayout(drawerLayout: DrawerLayout, navigationView: NavigationView){
+        drawerLayout.openDrawer(Gravity.RIGHT)
+        drawerLayout.let {
+            //왼쪽
+            if (it.isDrawerOpen(GravityCompat.END)) {
+                it.closeDrawer(GravityCompat.END)
+            }
+        }
+
+
     }
 
 }
