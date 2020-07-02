bc6411f1c42a46ec322c45c832005c2bbadf394b
leejieun1121 <lje4648@naver.com>
2020-07-03 00:32:15 +0900

    햄버거바 기능 클릭 -> 연결

---
+++ app/src/main/java/com/example/ulink/NoticeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.ulink
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NoticeActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notice)
+
+
+
+    }
+}
