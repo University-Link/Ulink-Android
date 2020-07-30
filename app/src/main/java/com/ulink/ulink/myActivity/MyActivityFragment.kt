3af8907884999721decf6637e9182c0ab6620e30
gch0925 <gch0925@naver.com>
2020-07-30 17:08:21 +0900

    [UPDATE] 회원인증 뷰

---
+++ app/src/main/java/com/ulink/ulink/myActivity/MyActivityFragment.kt
@@ -0,0 +1,19 @@
+package com.ulink.ulink.myActivity
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ulink.ulink.R
+
+class MyActivityFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_activity, container, false)
+    }
+
+}
