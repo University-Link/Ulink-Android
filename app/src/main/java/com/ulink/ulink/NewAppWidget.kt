68749e9da083780adcda0a36b69b58735f7c0feb
gch0925 <gch0925@naver.com>
2020-08-04 14:24:51 +0900

    [UPDATE] 위젯 작업중

--- app/src/main/java/com/ulink/ulink/NewAppWidget.kt
+++ app/src/main/java/com/ulink/ulink/NewAppWidget.kt
@@ -7,8 +7,13 @@ import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
 import android.content.ComponentName
+import android.view.LayoutInflater
 
 import com.ulink.ulink.fragment.TimeTableFragment
+import com.ulink.ulink.repository.Subject
+import com.ulink.ulink.repository.TimeTable
+import com.ulink.ulink.timetable.TimeTableDrawer
+import com.ulink.ulink.timetable.TimeTableDrawerWidget
 
 
 /**
@@ -16,6 +21,7 @@ import com.ulink.ulink.fragment.TimeTableFragment
  * App Widget Configuration implemented in [NewAppWidgetConfigureActivity]
  */
 class NewAppWidget : AppWidgetProvider() {
+
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         // AppWidgetProviderInfo의 updatePeriodMillis 속성에 의해 정의된 간격으로 앱 위젯을 업데이트하기 위해 호출
@@ -43,8 +49,14 @@ class NewAppWidget : AppWidgetProvider() {
 
 internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
     // val widgetText = loadTitlePref(context, appWidgetId)
+
     val views = RemoteViews(context.packageName, R.layout.new_app_widget)
     //views.setTextViewText(R.id.appwidget_text, widgetText)
+
+    val timeTable =  TimeTable(id=185, semester="2020-1", name="hfh", isMain=1, startTime="09:00",endTime="20:30", subjectList= mutableListOf(Subject(id=293, name="스터디", startTime= listOf("18:15"), endTime= listOf("20:30"), day= listOf(0), place= listOf(), color=4, subject=false, credit=0.0f, professor="", course="", isSample=false, number="", subjectIdx=0)))
+
+    val drawer = TimeTableDrawerWidget(context, LayoutInflater.from(context),timeTable)
+    drawer.drawForWidget(views)
 
     //위젯 누르면 시간표로 이
     val intent = Intent(Intent.ACTION_VIEW)
@@ -53,6 +65,5 @@ internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManage
     val pi = PendingIntent.getActivity(context,0,intent,0)
     views.setOnClickPendingIntent(R.id.layout_timetablewhole,pi)
     appWidgetManager.updateAppWidget(appWidgetId, views)
-
 
 }
---
+++ app/src/main/java/com/ulink/ulink/timetable/TimeTableDrawerWidget.kt
@@ -0,0 +1,252 @@
+package com.ulink.ulink.timetable
+
+import android.content.Context
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.*
+import com.ulink.ulink.R
+import com.ulink.ulink.fragment.TimeTableFragment
+import com.ulink.ulink.getColors
+import com.ulink.ulink.repository.Subject
+import com.ulink.ulink.repository.TimeTable
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class TimeTableDrawerWidget(val context: Context, val layoutInflater: LayoutInflater) {
+
+    constructor(context: Context, layoutInflater: LayoutInflater, onClick: TimeTableFragment.subjectOnClick) : this(context, layoutInflater) {
+        this.onClick = onClick
+    }
+
+
+    constructor(context: Context, layoutInflater: LayoutInflater, timeTable: TimeTable) : this(context, layoutInflater) {
+        this.timeTable = timeTable
+    }
+
+    constructor(context: Context, layoutInflater: LayoutInflater, onClick: TimeTableFragment.subjectOnClick, timeTable: TimeTable) : this(context, layoutInflater, onClick) {
+        this.timeTable = timeTable
+    }
+
+    var onClick: TimeTableFragment.subjectOnClick? = null
+
+    var timeTable = TimeTable(0, "", "", 0, "", "")
+
+    var endhour = 18
+    var starthour = 9
+
+    var timecolumnwidth = 0.0f
+    var dayheight = 0.0f
+    var columnwidth = 0.0f
+    var rowheight = 0.0f
+
+
+    fun setMinMax(){
+        for (sub in timeTable.subjectList){
+            for(i in sub.startTime){
+                if ( formatToFloat(i) < starthour) {
+                    starthour = formatToFloat(i).toInt()
+                }
+            }
+            for (k in sub.endTime){
+                if ( formatToFloat(k) > endhour){
+                    endhour = formatToFloat(k).toInt() + 1
+                }
+            }
+        }
+    }
+
+
+    fun drawForWidget(remoteViews : RemoteViews){
+
+        remoteViews.removeAllViews(R.id.timetable_root)
+        remoteViews.removeAllViews(R.layout.timetable_day_row)
+
+        drawDayRow(remoteViews)
+        drawHorizontalLine(remoteViews)
+        drawTimeColumn(remoteViews)
+        drawColums(remoteViews)
+    }
+
+    fun drawDayRow(remoteViews: RemoteViews) {
+        val dayOfWeek = 5
+        val daylist = arrayListOf("월", "화", "수", "목", "금", "토", "일")
+
+        val rvIndex = RemoteViews(context.packageName, R.layout.cell_top_left_index_widget)
+        remoteViews.addView(R.id.layout_dayrow, rvIndex)
+
+        for (i in 0 until dayOfWeek) {
+            val rvDay = RemoteViews(context.packageName, R.layout.cell_day_widget)
+            rvDay.setTextViewText(R.id.tv_cellday, daylist[i])
+            rvDay.setInt(R.id.tv_cellday, "setBackgroundResource", R.drawable.bottom_line)
+
+            remoteViews.addView(R.id.layout_dayrow, rvDay)
+        }
+
+    }
+
+    fun drawHorizontalLine(remoteViews: RemoteViews) {
+
+        remoteViews.removeAllViews(R.id.timetable_grid)
+
+        val rvHorizontalRow =RemoteViews(context.packageName, R.layout.timetable_grid)
+        rvHorizontalRow.setFloat(R.id.layout_timetable_grid, "setWeightSum", 4 * (endhour - starthour).toFloat())
+
+        for (i in starthour..endhour) {
+            val row = RemoteViews(context.packageName, R.layout.cell_timeindex)
+            row.setInt(R.id.tv_cell, "setBackgroundResource", R.drawable.bottom_line)
+            rvHorizontalRow.addView(R.id.layout_timetable_grid, row)
+        }
+
+        remoteViews.addView(R.id.timetable_grid, rvHorizontalRow)
+    }
+
+
+    fun drawTimeColumn(remoteViews: RemoteViews) {
+
+        val rvTimeColumn = RemoteViews(context.packageName, R.layout.day_column_for_time)
+        rvTimeColumn.setFloat(R.id.day_column_root, "setWeightSum",4 * (endhour - starthour).toFloat() )
+
+        val simpleDateFormat = SimpleDateFormat("h")
+        val instance = Calendar.getInstance()
+
+        for (i in starthour..endhour) {
+            val rvTimeColumnEa = RemoteViews(context.packageName, R.layout.cell_timeindex_widget)
+            rvTimeColumnEa.setInt(R.id.layout_tv_cell_widget, "setGravity", Gravity.CENTER)
+            instance.set(Calendar.HOUR_OF_DAY, i)
+            rvTimeColumnEa.setTextViewText(R.id.tv_cell_widget, simpleDateFormat.format(instance.time))
+            rvTimeColumn.addView(R.id.day_column_root, rvTimeColumnEa)
+        }
+        remoteViews.addView(R.id.timetable_root, rvTimeColumn)
+    }
+
+    fun drawColums(remoteViews: RemoteViews) {
+        val dayOfWeek = 5
+
+        drawVerticalLine(remoteViews)
+
+        for (i in 0 until dayOfWeek) {
+//          맨위에 월화수목금
+            val rvLinearLayout = RemoteViews(context.packageName, R.layout.day_column_widget)
+
+            rvLinearLayout.setFloat(R.id.day_column_root, "setWeightSum", 4 * (endhour - starthour).toFloat())
+            drawColumn(rvLinearLayout, i)
+
+            remoteViews.addView(R.id.timetable_root, rvLinearLayout)
+            if (i < 4) {
+                drawVerticalLine(remoteViews)
+            }
+        }
+    }
+
+    fun drawColumn(remoteViews: RemoteViews, i: Int) {
+
+        var subjectstarttime = 0f
+        var presubjectendtimeortablestarttime = 0f
+
+        var subjectList : MutableList<Subject> = arrayListOf()
+
+        for (k in timeTable.subjectList){
+            for(m in 0 until k.day.size){
+                if (k.day[m] == i){
+                    val b = Subject(k.id,k.name, listOf(k.startTime[m]), listOf(k.endTime[m]), listOf(k.day[m]), listOf(k.place[m]), k.color,k.subject,k.credit,k.professor,k.course,k.isSample,"",k.subjectIdx)
+                    subjectList.add(b)
+                }
+
+            }
+        }
+
+        subjectList.retainAll { !it.isSample }
+        subjectList.sortBy {formatToFloat(it.startTime[0])}
+        for (a in 0 until subjectList.size) {
+            if (a == 0) {
+                subjectstarttime = formatToFloat(subjectList[a].startTime[0]) * 4
+                presubjectendtimeortablestarttime = (starthour * 4).toFloat()
+            } else {
+                subjectstarttime = formatToFloat(subjectList[a].startTime[0]) * 4
+                presubjectendtimeortablestarttime = formatToFloat((subjectList[a - 1].endTime[0])) * 4
+            }
+
+            DrawDummy(remoteViews, subjectstarttime - presubjectendtimeortablestarttime)
+            DrawSubject(remoteViews, subjectList[a])
+        }
+
+    }
+
+
+    private fun DrawDummy(remoteViews: RemoteViews, i: Float) {
+//        if (i > 0) {
+//            val view = layoutInflater.inflate(R.layout.cell_subject_widget, linearLayout, false) as LinearLayout
+//            view.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, i)
+//            linearLayout.addView(view)
+//        }
+    }
+
+    private fun devideSubjects(remoteViews: RemoteViews) {
+
+        val rvView = RemoteViews(context.packageName, R.layout.cell_subject_widget)
+        val view = remoteViews.apply(context, null)
+
+        Log.d("tag", view.toString())
+
+        val cell = view.findViewById<TextView>(R.id.tv_cell_subject)
+        cell.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 0.25.toFloat())
+
+
+
+        remoteViews.addView(R.id.timetable_root, rvView)
+
+    }
+
+    fun DrawSubject(remoteViews: RemoteViews, subject: Subject) {
+
+//        devideSubjects(remoteViews)
+////        cell = 0일때 오류
+//        val rvCellLayout = RemoteViews(context.packageName, R.layout.cell_subject_widget)
+//
+//
+//        val celllayout = layoutInflater.inflate(R.layout.cell_subject, linearLayout, false) as LinearLayout
+//        celllayout.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0,
+//                4 * (formatToFloat(subject.endTime[0]) - formatToFloat(subject.startTime[0])) - 0.5f)
+//
+//        celllayout.setBackgroundResource(getColors(subject.color))
+//        celllayout.findViewById<TextView>(R.id.tv_cell_subject).text = subject.name
+//        celllayout.findViewById<TextView>(R.id.tv_cell_custom).apply {
+//            text = subject.place[0]
+//        }
+//
+//        remoteViews.addView(celllayout)
+//        devideSubjects(linearLayout)
+    }
+
+
+    fun drawVerticalLine(remoteViews: RemoteViews) {
+
+        val rvVerticalLines = RemoteViews(context.packageName, R.layout.timetable_verticallayout)
+        rvVerticalLines.setFloat(R.id.layout_line_vertical, "setWeightSum", 4 * (endhour - starthour).toFloat())
+
+
+        for (i in starthour..endhour) {
+
+            val rvVerticalLine = RemoteViews(context.packageName, R.layout.cell_timeindex)
+            rvVerticalLine.setInt(R.id.tv_cell, "setBackgroundResource", R.drawable.bg_vertical_bottom_line)
+
+            rvVerticalLines.addView(R.id.layout_line_vertical, rvVerticalLine)
+        }
+
+        remoteViews.addView(R.id.timetable_root, rvVerticalLines)
+    }
+
+
+    fun dptopx(dp: Int): Float {
+        val metrics = context.resources.displayMetrics
+        return dp * ((metrics.densityDpi.toFloat()) / DisplayMetrics.DENSITY_DEFAULT)
+    }
+
+    fun formatToFloat(time: String): Float {
+        val timesplit = time.split(":")
+        return timesplit[0].toFloat() + (timesplit[1].toFloat() - timesplit[1].toFloat() % 15) / 60
+    }
+}
