95e1c62921977964fecf7e29161aca3a05b655b4
kym1924 <kym1924@naver.com>
2020-08-07 17:16:43 +0900

    [UPDATE] 공지 뷰 80% 완성

--- app/src/main/java/com/ulink/ulink/fragment/UlinkFragment.kt
+++ app/src/main/java/com/ulink/ulink/fragment/UlinkFragment.kt
@@ -10,7 +10,7 @@ import android.view.ViewGroup
 import com.ulink.ulink.ClassRecycler.ClassAdapter
 import com.ulink.ulink.ClassRecycler.ClassData
 import com.ulink.ulink.R
-import com.ulink.ulink.Ulink.BoardSearchActivity
+import com.ulink.ulink.Ulink.BoardSearchRecycler.BoardSearchActivity
 import com.ulink.ulink.Ulink.UlinkUlinkBoardActivity
 import com.ulink.ulink.Ulink.UlinkInsideActivity
 import com.ulink.ulink.Ulink.UlinkUniversityBoardActivity
@@ -96,7 +96,6 @@ class UlinkFragment : Fragment() {
         btn_search.setOnClickListener {
             val intent = Intent(getActivity(), BoardSearchActivity::class.java)
             startActivity(intent)
-
         }
     }
 
